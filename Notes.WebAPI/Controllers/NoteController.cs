300ea8c6b4878adb8c7c22c3bf06a5f08e6b33a6
Clav4ik <89039462+Clav4ik@users.noreply.github.com>
2023-05-29 23:17:46 +0300

    Add_CRUD_Notes

---
+++ Notes.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,159 @@
+﻿using AutoMapper;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Application.Notes.Commands.CreateNote;
+using Notes.Application.Notes.Commands.UpdateNote;
+using Notes.Application.Notes.Commands.DeleteCommand;
+using Notes.WebAPI.Models;
+
+
+namespace Notes.WebAPI.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    //[ApiVersionNeutral]
+    [Produces("application/json")]
+    [Route("api/{version:apiVersion}[controller]")]
+    public class NoteController : BaseController
+    {
+        private readonly IMapper _mapper;
+
+        public NoteController(IMapper mapper) => _mapper = mapper;
+
+        /// <summary>
+        /// Gets the list of notes
+        /// </summary>
+        /// <remarks>
+        /// Sample request;
+        /// GET /note
+        /// </remarks>
+        /// <returns>Returns NoteListVm</returns>
+        /// <response code="200">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+        [HttpGet]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery
+            {
+                UserId = UserId
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+
+        /// <summary>
+        /// Gets the note by id
+        /// </summary>
+        /// <remarks>
+        /// Sample request;
+        /// GET /note/1C67B10F-6A73-4416-A248-8DF3A5819985
+        /// </remarks>
+        /// <param name="id"></param>
+        /// <returns>Returns NoteDetailsVm</returns>
+        /// <response code="200">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+        [HttpGet("{id}")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
+        {
+            var query = new GetNoteDetailsQuery
+            {
+                UserId = UserId,
+                Id = id
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+
+        /// <summary>
+        /// Creates the note
+        /// </summary>
+        /// <remarks>
+        /// Sample request;
+        /// POST /note
+        ///  {
+        ///     "title": "Test title",
+        ///     "details": "Test details"
+        /// }
+        /// </remarks>
+        /// <param name="CreateNoteDto"> CreateNoteDto object</param>
+        /// <returns>Returns NoteDetailsVm</returns>
+        /// <response code="201">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+        [HttpPost]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateNoteDto createNoteDto)
+        {
+            var command = _mapper.Map<CreateNoteCommand>(createNoteDto);
+            command.UserId = UserId;
+            var noteId = await Mediator.Send(command);
+            return Ok(noteId);
+        }
+
+        /// <summary>
+        /// Update the note
+        /// </summary>
+        /// <remarks>
+        /// Sample request;
+        /// PuT /note
+        ///  {
+        ///     "title": "update Test title"
+        /// }
+        /// </remarks>
+        /// <param name="updateNoteDto"> updateNoteDto object</param>
+        /// <returns>Returns NoContent</returns>
+        /// <response code="204">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+        [HttpPut]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Update([FromBody] UpdateNoteDto updateNoteDto)
+        {
+            var command = _mapper.Map<UpdateNoteCommand>(updateNoteDto);
+            command.UserId = UserId;
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Delete the note
+        /// </summary>
+        /// <remarks>
+        /// Sample request;
+        /// DELETE /note/61BD316D-1825-4ECB-9254-BA95B42F29D4
+        /// </remarks>
+        /// <param name="id"> Id of the note(guid)</param>
+        /// <returns>Returns NoContent</returns>
+        /// <response code="204">Success</response>
+        /// <response code="401">If the user is unauthorized</response>
+        [HttpDelete("{id}")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var command = new DeleteNoteCommand
+            {
+                Id = id,
+                UserId = UserId
+            };
+            await Mediator.Send(command);
+            return NoContent();
+
+        } 
+        
+    }
+}
